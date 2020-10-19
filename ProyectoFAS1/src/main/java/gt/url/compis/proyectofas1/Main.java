77a92c2fb7196e0f7d2cf77aa46b5b8b6a177a8d
Alle <allecan52@gmail.com>
2020-10-19 16:34:50 -0600

    rec texto cod

--- ProyectoFAS1/src/main/java/gt/url/compis/proyectofas1/Main.java
+++ ProyectoFAS1/src/main/java/gt/url/compis/proyectofas1/Main.java
@@ -5,11 +5,14 @@
  */
 package gt.url.compis.proyectofas1;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,13 +26,41 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = "";
+        String texto = "";
+        String fin = "end";
+        System.out.println("Escriba: ");
+
+        System.out.println(input);
+        while (input != "end") {
+
+            if (input.equals("end")) {
+                break;
+            } else {
+                input = in.nextLine();
+                texto = texto + "\n" + input;
+
+            }
+        }
+
         try {
-            Lexico lex = new Lexico(new InputStreamReader(new FileInputStream("codigo.txt")));
-            lex.yylex();
+            File archivo = new File("prueba2a.txt");
+            PrintWriter writer;
+            writer = new PrintWriter(archivo);
+            writer.print(texto);
+            writer.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+//        try {
+//            Lexico lex = new Lexico(new InputStreamReader(new FileInputStream("codigo.txt")));
+//            lex.yylex();
+//        } catch (FileNotFoundException ex) {
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (IOException ex) {
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        }
     }
 }
